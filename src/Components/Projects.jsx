24aa7498f995c3e74ed85b26168e9b2cdcdeb653
Karolinegaia <drakarolinegaia@gmail.com>
2022-12-20 15:18:54 -0300

    Updating the project

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -1,8 +1,12 @@
+import "../Styles/projects.sass"
+
+
 const Projects = () => {
     return <section className="projects-container">
         <h2>Projetos</h2>
-        <p>Projeto Dindin</p>
-        <p>Projeto Corrier</p>
+        <p>Os principais projetos possuem HTML5, CSS, JavaScript, React, Typescript, NodeJs e MySQL.</p>
+        <p>Você consegue ver o projetos no botão abaixo, dentro do meu GitHub</p>
+                
         <a href="https://github.com/Karolinegaia" className="btn">
             Ver Projetos
         </a>

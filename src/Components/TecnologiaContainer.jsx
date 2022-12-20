6b3dc6107649af0d6c0654abe1c2e4090da0d27e
Karolinegaia <drakarolinegaia@gmail.com>
2022-12-20 13:15:36 -0300

    Adding the icons

--- src/Components/TecnologiaContainer.jsx
+++ src/Components/TecnologiaContainer.jsx
@@ -1,6 +1,15 @@
 import {DiHtml5, DiCss3, DiJsBadge, DiNodejsSmall, DiMysql, DiReact} from "react-icons/di"
 import "../Styles/Tecnologia.sass"
 
+const technologies = [
+    { id: "html", name: "HTML5", icon: <DiHtml5 /> },
+    { id: "css", name: "CSS3", icon: <DiCss3 /> },
+    { id: "js", name: "JavaScript", icon: <DiJsBadge /> },
+    { id: "node", name: "Node.js", icon: <DiNodejsSmall /> },
+    { id: "mysql", name: "MySQL", icon: <DiMysql /> },
+    { id: "react", name: "React", icon: <DiReact /> },
+  ];
+
 const Tecnologia = () => {
     return
 }
